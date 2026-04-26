ad20f9a769565e7919c959f81a30d1fd1f2a6d18
John-Swindell <jswindell28@gmail.com>
2026-04-25 23:07:17 -0500

    add reload all from disk and all for visual > block

---
+++ bash/quick_copy.sh
@@ -0,0 +1,7 @@
+# configure this for your git repo location. 
+# allows for leaving the .ideavimrc at root for all IDEs without additional config.
+set -e
+
+cp ~/.ideavimrc ~/Documents/ideavim_conf
+echo "Copied '.ideavimrc' to '/Documents/ideavim_conf'"
+
